a3f06b70269d2c1c12be19aa5ee0501893da2910
joel-joshy <joel@sayonetech.com>
2025-07-25 19:00:38 +0530

    deployment

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Email (SendGrid or others)
     MAIL_SERVER = os.getenv('MAIL_SERVER')
